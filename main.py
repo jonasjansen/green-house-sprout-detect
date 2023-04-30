dbd7ca283ac0c9735a808c77b846fbfe2093b4ec
Jonas Jansen <git@jonasjansen.de>
2023-04-30 12:17:39 +0200

    Train model for sprout detection.

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import os
+import os.path
+import random
+import shutil
 
 
-# Press the green button in the gutter to run the script.
+def prepare_files_for_dataset():
+    files = os.listdir('data/training/picture_raw/sprout')
+    random.shuffle(files)
+    sample = files[:899]
+    src = 'data/training/picture_raw/sprout'
+    dest = 'data/training/picture/sprout'
+
+    for file_name in sample:
+        full_file_name = os.path.join(src, file_name)
+        if os.path.isfile(full_file_name):
+            shutil.copy(full_file_name, dest)
+
+
+def count_files():
+    print("Seed pictures raw", sum(len(files) for _, _, files in os.walk('data/training/picture_raw/seed')))
+    print("Seed pictures", sum(len(files) for _, _, files in os.walk('data/training/picture/seed')))
+    print("Sprout pictures raw", sum(len(files) for _, _, files in os.walk('data/training/picture_raw/sprout')))
+    print("Sprout pictures", sum(len(files) for _, _, files in os.walk('data/training/picture/sprout')))
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    prepare_files_for_dataset()
+    count_files()
